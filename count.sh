63eee25188df52571afe9d69b7f3e8ec6460d600
My name <517344511@qq.com>
2020-08-10 17:03:50 +0800

    第七版

--- count.sh
+++ count.sh
@@ -1,63 +1,77 @@
 #!/bin/bash
 ###################
-read  -p  '请输入项目名称：'   name
-read  -p  '请输入要统计的代码路径：'   path
-read  -p  '数据库版本(mysql/oracle)：' sqlstate
+#read  -p  '请输入项目名称：'   name
+#read  -p  '请输入要统计的代码路径：'   path
+#read  -p  '数据库版本(mysql/oracle)：' sqlstate
+date=`date +%Y%m%d`
 
-if [ -z "$path" ] || [ -z "$sqlstate" ];then
-  echo "你倒是输入参数啊！"
-  exit 1
-elif [ "$sqlstate" != "mysql" ] && [ "$sqlstate" != "oracle" ];then
-  echo "数据库版本请输入mysql或oracle"
-  exit 2
-fi
+if [[ ! $3 ]];then
+  echo "错误，已退出"
+  exit 168
+else
+  echo "开始执行..."
 
-if [[ -f $path/$name-count.txt ]];then
-  rm -f $path/$name-count.txt
-fi
+  if [ -z "$2" ] || [ -z "$3" ];then
+    echo "你倒是输入参数啊！"
+    exit 1
+  elif [ "$3" != "mysql" ] && [ "$3" != "oracle" ];then
+    echo "数据库版本请输入mysql或oracle"
+    exit 2
+  fi
+  
+  if [[ -f $PWD/$1-count.txt ]];then
+    rm -f $PWD/$1-count.txt
+  fi
+ 
+  if [[ -f $PWD/$1-$date-count2.txt ]];then
+    rm -f $PWD/$1-$date-count2.txt
+  fi
+ 
+  for i in {"*.xml","*.sql","*.java","*.properties","*.js","*.gradle","*.html","Abstract*.java","*VO.java","*BaseSQL.xml","*BaseSQL_"$3".xml"}
+    do
+      find "$2" -name "$i" | xargs ls -l | gawk '{print $5}' | gawk 'BEGIN {SUM=0} {SUM+=$1} END {print SUM}' >> $2/"$i"-4.txt
+      for l in `find "$2" -name "$i"`
+        do
+          sed -n '/^\s*$/p' $l |wc -l >> $2/"$i"-1.txt
+          sed -n "/^\s*\//p" $l | wc -l >> $2/"$i"-2.txt
+          sed -n "/^\s*#/p" $l | wc -l >> $2/"$i"-2.txt
+          sed -n "/^\s*\*/p" $l | wc -l >> $2/"$i"-2.txt
+          grep -Pzo '<!(.|[\r\n])*?>' $l | wc -l >> $2/"$i"-2.txt
+          cat $l | wc -l >> $2/"$i"-3.txt
+  
+          if [[ -f $2/"$i"-1.txt ]];then
+            a=`cat $2/"$i"-1.txt | awk '{sum+=$1} END {print sum}'`
+          else
+            a=0
+          fi
+  
+          if [[ -f $2/"$i"-2.txt ]];then
+            b=`cat $2/"$i"-2.txt | awk '{sum+=$1} END {print sum}'`
+          else
+            b=0 
+          fi
+  
+          if [[ -f $2/"$i"-3.txt ]];then
+            c1=`cat $2/"$i"-3.txt | awk '{sum+=$1} END {print sum}'`
+            num=`find "$2" -name "$i" |wc -l`
+            c=$[ $c1 + $num ]
+              if [[ "${i}" =~ gradle$ ]];then
+                c=$[ $c - 1 ]
+              fi
+          else
+            c1=0
+            num=0
+            c=0
+          fi
+  
+          d=$[$c - $b - $a]
+          e=`cat $2/"$i"-4.txt`
 
-for i in {"*.gradle","*.xml","*.sql","*.java","*.properties","*.js","*.html","Abstract*.java","*VO.java","*BaseSQL.xml","*BaseSQL_"$sqlstate".xml",}
-  do
-    find "$path" -name "$i" | xargs ls -l |gawk '{print $5}' |gawk 'BEGIN {SUM=0} {SUM+=$1} END {print SUM}' >> $path/"$i"-4.txt
-    for l in `find "$path" -name "$i"`
-      do
-        sed -n '/^\s*$/p' $l |wc -l >> $path/"$i"-1.txt
-        sed -n "/^\s*\//p" $l | wc -l >> $path/"$i"-2.txt
-        sed -n "/^\s*#/p" $l | wc -l >> $path/"$i"-2.txt
-        sed -n "/^\s*\*/p" $l | wc -l >> $path/"$i"-2.txt
-        grep -Pzo '<!(.|[\r\n])*?>' $l | wc -l >> $path/"$i"-2.txt
-        cat $l | wc -l >> $path/"$i"-3.txt
-
-        if [[ -f $path/"$i"-1.txt ]];then
-          a=`cat $path/"$i"-1.txt | awk '{sum+=$1} END {print sum}'`
-        else
-          a=0
-        fi
-
-        if [[ -f $path/"$i"-2.txt ]];then
-          b=`cat $path/"$i"-2.txt | awk '{sum+=$1} END {print sum}'`
-        else
-          b=0 
-        fi
-
-        if [[ -f $path/"$i"-3.txt ]];then
-          c1=`cat $path/"$i"-3.txt | awk '{sum+=$1} END {print sum}'`
-          num=`find "$path" -name "$i" |wc -l`
-          c=$[ $c1 + $num ]
-            if [[ "${i}" =~ gradle$ ]];then
-              c=$[ $c - 1 ]
-            fi
-        else
-          c1=0
-          num=0
-          c=0
-        fi
-
-        d=$[$c - $b - $a]
-        e=`cat $path/"$i"-4.txt`
-      done
-
-cat << EOF >> $name-count.txt
+        done
+            
+            echo $d >> $2/$1-$date-code.txt
+ 
+cat << EOF >> $1-$date-count.txt
 "$i"
 文件数：     $num
 文件大小:    $e
@@ -68,9 +82,28 @@ cat << EOF >> $name-count.txt
 
 EOF
 
-    rm -f $path/"$i"-1.txt $path/"$i"-2.txt $path/"$i"-3.txt $path/"$i"-4.txt
+    num=0
+    a=0
+    b=0
+    c=0
+    d=0
+    e=0
 
-  done
+      rm -f $2/"$i"-1.txt $2/"$i"-2.txt $2/"$i"-3.txt $2/"$i"-4.txt
+  
+    done
 
-cat $PWD/$name-count.txt
+  sed -i '5d' $2/$1-$date-code.txt
+  f=`cat $2/$1-$date-code.txt | awk 'NR==1,NR==6{sum+=$1} END {print sum}'`
+  g=`cat $2/$1-$date-code.txt | awk 'NR==4,NR==10{sum+=$1} END {print sum}'`
+
+  echo -e "代码行数1:\t$f" > $PWD/$1-$date-count2.txt 
+  echo -e "代码行数2:\t$g" >> $PWD/$1-$date-count2.txt
+
+  cat $PWD/$1-$date-count.txt
+  cat $PWD/$1-$date-count2.txt
+  rm -f $2/$1-$date-code.txt
+
+  fi
+
 ###################
